bea15fa0ff9249786ab011b13fea94a55c2ce0cc
Mahmoud Elshahat <53579447+MahmoudElshahat@users.noreply.github.com>
2021-11-03 13:23:29 +0200

    Add files via upload

--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use app\Http\Middleware;
 use Illuminate\Support\Facades\Auth;
 use App\Admin;
+use App\Http\Controllers\admin\categoriController;
 ###############3
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\admin\languagesController;
@@ -19,35 +20,63 @@ use App\Http\Controllers\admin\languagesController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-// ################## start admin Dashboard routes ######################
-// define('pagination_count',10);
-Route::group(['namespace' => 'admin','middleware' => 'auth:admin'], function () {
-    Route::get('/', 'dashboardController@index')->name('admin.dashboard');
-
-// >>>>>> start language route <<<<<<<<<<<<<<
-Route::group(['prefix'=>'languages'],function(){
-    Route::get('/','languagesController@index')->name('admin.languages');
-    Route::get('create','languagesController@create')->name('lang.create');
-    Route::post('insert','languagesController@store')->name('lang.store');
-    Route::get('edit/{id}','languagesController@edit')->name('lang.edite');
-    Route::post('update/{id}','languagesController@update')->name('lang.update');
-});
-
-}
-// >>>>>> end language route <<<<<<<<<<<<<<<<<
-
-
-
-);
-
-####################### start admin login routes #################################
+###################### start admin login routes #################################
 Route::group(['namespace'=>'admin','middleware' => 'guest:admin'], function () {
     Route::get('login', 'LoginController@getLogin')->name('get.admin.login');
     Route::post('login', 'LoginController@login')->name('admin.login');
 
     Route::get('backHome','LoginController@backhome')->name('backhome');
 
-});
+});// end adminlogin route
+// ################## start admin Dashboard routes ######################
+define('pagination_count',10);
+Route::group(['namespace' => 'admin','middleware' => 'auth:admin'], function () {
+    Route::get('/', 'dashboardController@index')->name('admin.dashboard');
+
+// >>>>>>>>>>>>>>>>>>>>>>>> start language route <<<<<<<<<<<<<<
+Route::group(['prefix'=>'languages'],function(){
+
+    Route::get('/','languagesController@index')->name('admin.languages');
+
+    Route::get('create','languagesController@create')->name('lang.create');
+
+    Route::post('insert','languagesController@store')->name('lang.store');
+
+    Route::get('edite/{id}','languagesController@edite')->name('admin.languages.edite');
+
+    Route::GET('update/{id}','languagesController@update')->name('admin.languages.update');
+
+    Route::GET('delete/{id}','languagesController@delete')->name('admin.languages.delete');
+
+});// >>>>>> end language route <<<<<<<<<<<
+// ################ start categori routes ##################33
+Route::group(['prefix'=>'categori'],function(){
+    Route::get('/','categoriController@index')->name('all.categori');
+
+    Route::get('create','categoriController@create')->name('create.categori');
+
+    Route::post('insert','categoriController@insert')->name('insert.categori');
+
+    Route::get('edite/{id}','categoriController@edite')->name('edite.ctegori');
+
+    Route::POST('update/{id}','categoriController@update')->name('update.categori');
+
+    Route::get('delete/{id}','categoriController@delete')->name('delete.categori');
+
+});// end categori route
+// ############## starts items route #################################
+Route::group(['prefix'=>'items'],function(){
+
+Route::get('/','itemsController@index')->name('all.items');
+
+
+});//end items route
+
+
+});// end main group auth admin  <<<<<<<<<<<
+
+
+
 ###################   ####################
 
 
